f5ab83f7dc4c4fe6fd82449f2d0a13d66817ac1f
jbb <jbbernard@openwt.com>
2015-07-22 06:52:57 +0100

    add param URL encode

--- lib/active_rest_client/request.rb
+++ lib/active_rest_client/request.rb
@@ -211,7 +211,7 @@ module ActiveRestClient
         matches.each do |token|
           token = token.first[1,999]
           target = @get_params.delete(token.to_sym) || @post_params.delete(token.to_sym) || @get_params.delete(token.to_s) || @post_params.delete(token.to_s) || ""
-          @url.gsub!(":#{token}", target.to_s)
+          @url.gsub!(":#{token}", CGI.escape(target.to_s))
         end
       end
     end
